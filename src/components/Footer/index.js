a7e8a34dd67f16e9bc7f24d8b8228fef5f674c52
seacrest3 <o7716442@gmail.com>
2023-03-19 10:56:35 +0000

    added resume link and logo at footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer">
       <span>Lamda 2023</span>
       <div class="col-lg-1 col-md-2 col-sm-2">
-        <a href="tel:+441869168168" title="+44 1869 168168"><img class="contactImage" src="images/phone.png" alt="phone logo"/></a>
+        <a href="tel:+44 7716876577" title="+44 1869 168168"><img class="contactImage" src="images/phone.png" alt="phone logo"/></a>
       </div>
       <div class="col-lg-1 col-md-2 col-sm-2">
         <a href="mailto:o7716442@gmail.com" title="o7716442@gmail.com"><img class="contactImage" src="images/email.png" alt="email logo"/></a>
@@ -16,6 +16,9 @@ function Footer() {
       </div>
       <div class="col-lg-1 col-md-2 col-sm-2">
         <a href="https://hk.linkedin.com/in/kelvin-lee-8a9929146" title="https://hk.linkedin.com/in/kelvin-lee-8a9929146"><img class="contactImage" src="images/LinkedIn.png" alt="LinkedIn logo"/></a>
+      </div>
+      <div class="col-lg-1 col-md-2 col-sm-2">
+        <a href="https://drive.google.com/file/d/1zClOUx388Hq16WJjxkTDxobbfCcNBpbP/view?usp=share_link" title="https://drive.google.com/file/d/1zClOUx388Hq16WJjxkTDxobbfCcNBpbP/view?usp=share_link"><img class="contactImage" src="images/resume.png" alt="Resume logo"/></a>
       </div>
     </footer>
   );
