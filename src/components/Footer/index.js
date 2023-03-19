856152c73f0d7411979b0b09d746a2f477ead3a9
seacrest3 <o7716442@gmail.com>
2023-03-19 10:36:17 +0000

    added logo of CV and contact information at footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,9 +4,20 @@ import "./style.css";
 function Footer() {
   return (
     <footer className="footer">
-      <span>Pupster 2017</span>
+      <span>Lamda 2023</span>
+      <div class="col-lg-1 col-md-2 col-sm-2">
+        <a href="tel:+441869168168" title="+44 1869 168168"><img class="contactImage" src="images/phone.png" alt="phone logo"/></a>
+      </div>
+      <div class="col-lg-1 col-md-2 col-sm-2">
+        <a href="mailto:o7716442@gmail.com" title="o7716442@gmail.com"><img class="contactImage" src="images/email.png" alt="email logo"/></a>
+      </div>
+      <div class="col-lg-1 col-md-2 col-sm-2">
+        <a href="https://github.com/seacrest3" title="https://github.com/seacrest3"><img class="contactImage" src="images/github.png" alt="GitHub logo"/></a>
+      </div>
+      <div class="col-lg-1 col-md-2 col-sm-2">
+        <a href="https://hk.linkedin.com/in/kelvin-lee-8a9929146" title="https://hk.linkedin.com/in/kelvin-lee-8a9929146"><img class="contactImage" src="images/LinkedIn.png" alt="LinkedIn logo"/></a>
+      </div>
     </footer>
   );
 }
-
 export default Footer;
